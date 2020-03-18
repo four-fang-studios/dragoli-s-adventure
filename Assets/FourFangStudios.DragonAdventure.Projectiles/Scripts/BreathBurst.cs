083ac736f14e633914bf8612e595ef05561bd16d
ankien <andrew_kien12@yahoo.com>
2020-03-17 20:43:51 -0400

    added layers and breath collision

---
+++ Assets/FourFangStudios.DragonAdventure.Projectiles/Scripts/BreathBurst.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace FourFangStudios.DragonAdventure.Projectiles 
+{
+  public class BreathBurst : MonoBehaviour
+  {
+    #region Destroy
+    private void DestroyObjectDelayed() 
+    {
+      Destroy(gameObject, 3);
+    }
+
+    private void OnCollisionEnter() 
+    {
+      Destroy(gameObject);
+    }
+    #endregion
+
+    #region Monobehavior Events
+    protected void Start() 
+    {
+      DestroyObjectDelayed();
+    }
+    #endregion
+
+    #region Inspector / Breath
+
+    /// <summary>
+    /// Type of breath.
+    /// </summary>
+    [SerializeField] BreathType breathType;
+
+    #endregion
+  }
+}
+
+
