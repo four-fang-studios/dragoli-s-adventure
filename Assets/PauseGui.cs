e54e076d53d5479e4bf5c7bec4761a5ea909821c
Benjamin La Madrid <got12g@gmail.com>
2020-04-11 22:24:05 -0400

    pause/settings menues +sample scene 'guidebug' with ui canvas +new input axis 'toggle pause' (so it isn't mapped to 'Cancel')

---
+++ Assets/PauseGui.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGui : MonoBehaviour
+{
+
+  public static bool IsGamePaused = false;
+  public GameObject pausePanel;
+  public GameObject settingsPanel;
+
+  protected void Update()
+  {
+      if (Input.GetButtonDown("Toggle Pause")) {
+        if (!IsGamePaused) {
+          Pause();
+        } else {
+          Resume();
+        }
+      }
+  }
+
+  public void Pause() {
+    pausePanel.SetActive(true);
+    Time.timeScale = 0f;
+    IsGamePaused = true;
+  }
+
+  public void Resume() {
+    pausePanel.SetActive(false);
+    Time.timeScale = 1f;
+    IsGamePaused = false;
+  }
+
+  public void ViewSettings() {
+    settingsPanel.SetActive(true);
+    pausePanel.SetActive(false);
+  }
+
+  public void CloseSettings() {
+    pausePanel.SetActive(true);
+    settingsPanel.SetActive(false);
+  }
+
+  public void Quit() {
+    Application.Quit();
+  }
+}
