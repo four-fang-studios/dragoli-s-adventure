f6cee157901b3152c0a9b55fea09f8f0153e0f0f
Benjamin La Madrid <got12g@gmail.com>
2020-04-12 01:09:19 -0400

    added GreeterGui script for Rx testing

---
+++ Assets/FourFangStudios.DragonAdventure.Debug/Scripts/GreeterGui.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace FourFangStudios.DragonAdventure.Debug.Scripts
+{
+  public class GreeterGui : MonoBehaviour
+  {
+    [Inject] private IGreeter _greeter;
+    public UnityEngine.UI.Button greetingButton;
+    public UnityEngine.UI.Text greetingsCountLabel;
+
+     void Start()
+    {
+      this._greeter.Greets.Select(x => $"Greet count: {x.ToString()}").Subscribe(
+        (string msg) => {
+          UnityEngine.Debug.Log(msg);
+          this.greetingsCountLabel.text = msg;
+        }
+      );
+      this.greetingButton.onClick.AddListener(
+        () => { 
+          this._greeter.Greet("Homer");
+        }
+      );
+    }
+  }
+}
